e1c6e6174626577e92a80121b66601a07ba5f9f4
otmosina <otmosina@gmail.com>
2021-06-15 11:46:43 +0800

    add test to access & dry tokens class

--- lib/fresh_jwt/entity/access_token.rb
+++ lib/fresh_jwt/entity/access_token.rb
@@ -3,20 +3,16 @@ module FreshJwt
 
     class Token
       extend Dry::Initializer
-      option :name, proc(&:to_s), default: -> { 'access' }
+      option :name, proc(&:to_s), default: -> { self.class.to_s.split('::').last }
       option :token, proc(&:to_s)
       option :issued_at, proc(&:to_i), default: -> { Time.now.to_i }
     end
 
     class AccessToken < Token
-      extend Dry::Initializer
-
-      option :name, proc(&:to_s), default: -> { 'access' }
       option :payload, Payload, optional: true
     end
 
     class RefreshToken < Token
-      option :name, proc(&:to_s), default: -> { 'refresh' }
     end
 
 
