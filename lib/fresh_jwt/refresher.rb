9891d9117f7cfd3442d96b4466a2b3d52874d58e
otmosina <otmosina@gmail.com>
2021-06-27 11:33:44 +0800

    feat: add validate expiration to refresh token

--- lib/fresh_jwt/refresher.rb
+++ lib/fresh_jwt/refresher.rb
@@ -2,12 +2,16 @@
 
 module FreshJwt
   class Refresher
+    class TokenExpiredError < StandardError; end
     extend Dry::Initializer
     include Dry::Monads[:result, :do]
     option :repo, default: -> { Store.repo }
 
     def call(refresh_token)
       token_object = yield repo.find_by_token refresh_token
+      
+      yield expiration_validate token_object
+
       
       Success(token_object)
       #if token_object
@@ -17,6 +21,14 @@ module FreshJwt
       #end
       #yield validate token
 
+    end
+
+    def expiration_validate token_object
+      if token_object.expired?
+        Failure(error: TokenExpiredError.new)
+      else
+        Success()
+      end  
     end
 
     def validate token
--- spec/fresh_jwt/refresher_spec.rb
+++ spec/fresh_jwt/refresher_spec.rb
@@ -1,23 +1,44 @@
 # frozen_string_literal: true
-
+# кажется причина ошибок в спеках в том, что Memory obj шарится в разных местах
+# нужно видимо делать instance_double()
+include Dry::Monads[:result]
 RSpec.describe FreshJwt::Refresher do
   let(:unknown_token) { 'unknown_token' }
   let(:token) { SecureRandom.uuid }
+
   let(:repo) { FreshJwt::Store::Memory.new }
   before do
     FreshJwt::Store.repo = repo
+    FreshJwt::Store.repo.single_transaction token
   end
-  xit 'have not found a token' do
+  it 'have not found a token' do
     expect(described_class.new.call(unknown_token)).to be_failure
   end
 
   context 'when add token to store' do
-    before do
-      FreshJwt::Store.repo.single_transaction token
-    end
+    #before do
+      
+    #end
     it 'retuns success with token' do
       expect(described_class.new.call(token)).to be_success
-    end    
+    end
   end
+
+  context 'when invalid by differents reason' do
+    let(:subject) {described_class.new.call(token)}
+    before do
+      t = Time.at( Time.now.to_i + FreshJwt::Expiration::ACCESS + 1 )
+      Timecop.travel(t)    
+    end
+    it 'should return failure object coz token has expire' do
+      expect(subject).to be_failure
+    end
+
+    it 'should return concrete error coz token has expire' do
+      expect(subject.failure).to eq(error: described_class::TokenExpiredError.new)
+    end
+    
+  end
+
 
 end
