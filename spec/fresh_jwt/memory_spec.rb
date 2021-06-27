b5f791313f0a2eb0be12b105091794e9674caf7e
otmosina <otmosina@gmail.com>
2021-06-27 11:34:56 +0800

    comment section in memory coz dont use mixin

--- spec/fresh_jwt/memory_spec.rb
+++ spec/fresh_jwt/memory_spec.rb
@@ -28,6 +28,7 @@ RSpec.describe FreshJwt::Store::Memory do
     end
   end
 
+=begin
   # TODO: remove it coz mixin do not use anymore
   describe 'single_transaction mixin' do
     before do
@@ -37,23 +38,25 @@ RSpec.describe FreshJwt::Store::Memory do
       expect(memory_store.single_transaction(correct_token_object)).to be_kind_of(Success)
     end
 
-    it 'can find token after save via single transaction' do
+    xit 'can find token after save via single transaction' do
+      #require 'pry'; binding.pry
       memory_store.single_transaction(correct_token_object)
       expect(memory_store.find_by_token(correct_token_object.token).value!).to eq(correct_token_object.token)
     end
   end
-
+=end
   #it_behaves_like 'single_transaction mixin'
 
   describe 'use delegator for Decorate Store' do
-    let(:memory_store_decorated) {FreshJwt::Store::Decorator.new(memory_store)}
+    let(:memory_store_decorated) { FreshJwt::Store::Decorator.new(FreshJwt::Store::Memory.new) }
+
     it 'returns Success monad' do
-      expect(memory_store_decorated.single_transaction(correct_token_object)).to be_kind_of(Success)
+      expect(memory_store_decorated.single_transaction(correct_token_object.token)).to be_kind_of(Success)
     end
   
     it 'can find token after save via single transaction' do
-      memory_store_decorated.single_transaction(correct_token_object)
-      expect(memory_store_decorated.find_by_token(correct_token_object.token).value!).to eq(correct_token_object.token)
+      memory_store_decorated.single_transaction(correct_token_object.token)
+      expect(memory_store_decorated.find_by_token(correct_token_object.token).value!.token).to eq(correct_token_object.token)
     end    
   end
 end
